b51ae523be7e44b099b1fa90c1ac7616ab03201a
luizz567 <martins.567@hotmail.com>
2023-03-03 12:35:22 -0300

    Correção dos commits anteriores

---
+++ user_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum update --assumeyes
+yum install --assumeyes docker git
+systemctl enable --now docker
+DOCKER_CONFIG=/usr/local/lib/docker
+mkdir --parents $DOCKER_CONFIG/cli-plugins
+curl --location https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 --output $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+usermod --append --groups docker ec2-user
+cd /root/
+git clone https://github.com/luizz567/Compass.uol-Atividade-Docker.git
+cd Compass.uol-Atividade-Docker
+mkdir --parents /nfs/
+cat etc/fstab >> /etc/fstab
+mount -a
+docker compose up --detachS
